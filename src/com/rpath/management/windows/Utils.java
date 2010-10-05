20ef9d5cdb53d793088f3b3eaa5267963204bc79
Elliot Peele <https://issues.rpath.com/>
2010-10-05 01:28:01 -0400

    add utility method for joining strings

--- src/com/rpath/management/windows/Utils.java
+++ src/com/rpath/management/windows/Utils.java
@@ -38,4 +38,27 @@ public class Utils {
 	public static String[] slice(String[] input, int start) {
 		return slice(input, start, input.length);
 	}
+	
+	/**
+	 * Join a string on a separator.
+	 * @param input Input string array
+	 * @param delemeter String to insert between each element if the input
+	 */
+	public static String join(String[] input, String delemeter) {
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<input.length; i++) {
+			buff.append(input[i]);
+			if (i<input.length-1)
+				buff.append(delemeter);
+		}
+		return buff.toString();
+	}
+	
+	/**
+	 * Join a string on space
+	 * @param input Input string array
+	 */
+	public static String join(String[] input) {
+		return join(input, " ");
+	}
 }
